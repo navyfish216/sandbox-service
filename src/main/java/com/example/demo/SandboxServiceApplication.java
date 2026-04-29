f6903f46942763d6cd779526e10d129f96890be6
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-04-29 16:16:13 +0900

    設定値を共通化 ・プロパティの共通化した項目をコメントアウト ・共通プロパティを読み込み

--- src/main/java/com/example/demo/SandboxServiceApplication.java
+++ src/main/java/com/example/demo/SandboxServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:common.properties")
 public class SandboxServiceApplication {
 
 	public static void main(String[] args) {
