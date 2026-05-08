4478ce3a2c7a909308a3247be19913aba1ac3ca0
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-05-08 20:54:54 +0900

    【Java】Thymeleafで複数のフォームコントロールを持つ画面を作成

--- src/main/java/com/example/demo/sandbox/web/SandboxWebController.java
+++ src/main/java/com/example/demo/sandbox/web/SandboxWebController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.ArrayUtils;
 
 @Controller
 @RequestMapping("/web")
@@ -16,8 +17,8 @@ public class SandboxWebController {
 	@GetMapping({"", "/"})
 	public ModelAndView getIndex(ModelAndView mav) {
 		
-		 mav.addObject("msg", "名前を書いてください。");
-		 mav.setViewName("index");
+		mav.addObject("msg", "名前を書いてください。");
+		mav.setViewName("index");
 		
 		return mav;
 	}
@@ -25,8 +26,8 @@ public class SandboxWebController {
 	@PostMapping({"", "/"})
 	public ModelAndView postIndexForm(@RequestParam("text1") String str, ModelAndView mav) {
 		
-		 mav.addObject("msg", "こんにちは、" + str + "さん！");
-		 mav.setViewName("index");
+		mav.addObject("msg", "こんにちは、" + str + "さん！");
+		mav.setViewName("index");
 		
 		return mav;
 	}
@@ -42,4 +43,42 @@ public class SandboxWebController {
 		};
 	}
 	
+	@GetMapping({"/multi-form", "/multi-form/"})
+	public ModelAndView getMultiForm(ModelAndView mav) {
+		
+		mav.addObject("msg", "フォームを送信ください。");
+		mav.setViewName("multi-form/index");
+		
+		return mav;
+	}
+
+	@PostMapping({"/multi-form", "/multi-form/"})
+	public ModelAndView postMultiForm(
+			@RequestParam(required = false) boolean check1, 
+			@RequestParam(required = false) String radio1, 
+			@RequestParam(required = false) String select1, 
+			@RequestParam(required = false) String[] select2, 
+			ModelAndView mav) {
+		
+		String res = "";
+		
+		try {
+			res = "check:" + check1 
+					+ " radio:" + radio1 
+					+ " select:" + select1 
+					+ "\nselect2:";
+		} catch (NullPointerException e) {}
+
+		if (!ArrayUtils.isEmpty(select2)) {
+			res += String.join(", ", select2);
+		} else {
+			res += "no select";
+		}
+		
+		mav.addObject("msg", res);
+		mav.setViewName("multi-form/index");
+		
+		return mav;
+	}
+
 }
